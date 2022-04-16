173f21cdeecde779a5c6a01c71552ca5bed3b31f
chenyuheng <pass@chenyuheng.cn>
2022-04-16 23:37:29 +0800

    basic game logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
+const row_length = 15;
+const row_number = 6;
 const token_set = [
-    "He", "Li", "Be", "Na", "Mg", "Al", "Si",  "Cl", "Ca",
+    "He", "Li", "Be", "Na", "Mg", "Al", "Si",  "Cl", "Ca", "He", "Ne", "Ar",
     "H", "B", "C", "N", "O", "F", "P", "S", "K",
     "1", "2", "3", "4", "5", "6", 
     "=", "+", "(", ")"];
+
 const number_tokens = ["1", "2", "3", "4", "5", "6"];
+
+const keyboard_chars = [
+    ["H", "1", "2", "3", "4", "5", "6", "He"],
+    ["Li", "Be", "B", "C", "N", "O", "F", "Ne"],
+    ["Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar"],
+    ["K", "Ca", "+", "→", "(", ")", "←", "J"]
+]
+
+const icons = {
+    "←": {
+        "svg": `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
+                <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z" />
+                <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z" />
+            </svg>
+        `,
+        "color": "red"
+    },
+    "J": {
+        "svg": `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z" />
+            </svg>
+        `,
+        "color": "green"
+    }
+}
+
+var current_row = 0;
+var current_col = 0;
+var target_equation = "";
+var target_equation_tokens = [];
 
 Array.prototype.count = function(element) {
     return this.filter(x => x == element).length;
@@ -31,7 +66,7 @@ Object.prototype.add = function(key, number) {
     this[key] += number;
 }
 
-function equation_tokenizer(equation_str) {
+function equation_tokenizer(equation_str, length=-1) {
     let tokens = [];
     let i = 0;
     while (i < equation_str.length) {
@@ -49,6 +84,11 @@ function equation_tokenizer(equation_str) {
         if (!found) {
             console.error("tokenizer error: " + equation_str.substring(i));
             return;
+        }
+    }
+    if (length != -1) {
+        while (tokens.length < length) {
+            tokens.push("");
         }
     }
     return tokens;
@@ -178,7 +218,11 @@ function atoms_counter(items) {
 }
 
 // check if the equation is balanced
-function equation_checker(equation) {
+function equation_checker(equation_str) {
+    let equation = equation_parser(equation_str);
+    if (equation == null) {
+        return false;
+    }
     let left_count = atoms_counter(equation.left);
     let right_count = atoms_counter(equation.right);
     // console.log(left_count);
@@ -217,8 +261,119 @@ function get_equations() {
                 }
             }
             console.log(c);
+            while (true) {
+                let random = Math.floor(Math.random() * lines.length);
+                target_equation = lines[random].trim();
+                target_equation_tokens = equation_tokenizer(target_equation, row_length);
+                if (target_equation_tokens.length <= row_length) {
+                    break;
+                }
+            }
         }
     };
     r.open("GET", "data/equations.txt");
     r.send();
+}
+
+function extract_equation(row_no) {
+    return $("#row_" + row_no).text().replace("→", "=");
+}
+
+function handle_backspace() {
+    if (current_col > 0) {
+        current_col--;
+        let id = current_row + "_" + current_col;
+        $("#" + id).text("");
+        $("#" + id).attr("class", "empty grid");
+    }
+}
+
+function handle_submit() {
+    let submit_equation = extract_equation(current_row);
+    if (!equation_checker(submit_equation)) {
+        alert("invalid equation");
+        return;
+    }
+    let submit_equation_tokens = equation_tokenizer(submit_equation, row_length);
+    let col_no = 0;
+    for (; col_no < row_length; col_no++) {
+        let id = current_row + "_" + col_no;
+        if (submit_equation_tokens[col_no] == target_equation_tokens[col_no]) {
+            $("#" + id).attr("class", "hit grid");
+        } else if (target_equation_tokens.includes(submit_equation_tokens[col_no])) {
+            $("#" + id).attr("class", "partial-hit grid");
+        } else {
+            $("#" + id).attr("class", "missed grid");
+        }
+    }
+    current_row++;
+    current_col = 0;
+    if (current_row >= row_number) {
+        alert("the target equation is: " + target_equation);
+    }
+}
+
+function handle_equation_token(token) {
+    let id = current_row + "_" + current_col;
+    $("#" + id).text(token);
+    $("#" + id).attr("class", "staged grid");
+    current_col++;
+}
+
+function handle_button(token) {
+    if (token == "←") {
+        handle_backspace();
+    } else if (token == "J") {
+        handle_submit();
+    } else {
+        handle_equation_token(token);
+    }
+}
+
+function display_grids() {
+    let grids = $("#grids");
+    let row_no = 0;
+    for (; row_no < row_number; row_no++) {
+        let row = $("<div></div>");
+        row.attr("class", "row");
+        row.attr("id", "row_" + row_no);
+        grids.append(row);
+        let col_no = 0;
+        for (; col_no < row_length; col_no++) {
+            let grid = $("<div></div>");
+            grid.attr("class", "empty grid");
+            grid.attr("id", row_no + "_" + col_no);
+            row.append(grid);
+        }
+    }
+}
+
+function display_keyborad() {
+    let keyboard = $("#keyboard");
+    let row_no = 0;
+    for (; row_no < keyboard_chars.length; row_no++) {
+        let row = $("<div></div>");
+        row.attr("class", "row");
+        keyboard.append(row);
+        let col_no = 0;
+        for (; col_no < keyboard_chars[row_no].length; col_no++) {
+            let button = $("<div></div>");
+            button.attr("class", "button");
+            let char = keyboard_chars[row_no][col_no];
+            if (char in icons) {
+                button.html(icons[char].svg);
+                button.css("color", icons[char].color);
+            } else {
+                button.text(char);
+            }
+            button.attr("onclick", "handle_button('" + char + "');");
+            row.append(button);
+        }
+    }
+}
+
+function init() {
+    display_grids();
+    display_keyborad();
+    get_equations();
 }
